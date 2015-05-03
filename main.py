748907ce0be1f36ec3353093747b038406f52a78
Jason Matney <jamatney@gmail.com>
2015-05-03 02:50:28 -0400

    Update main.py

--- main.py
+++ main.py
@@ -93,17 +93,17 @@ class SpatialJoin:
             print "{0} is a type of {1} with a length of {2}"\
                 .format(field.name, field.type, field.length)
 
-    # def define_and_project(self, ofc):
-    #     self.output_feature_class = ofc
-    #     """Define spatial projection and create projected outfile"""
-    #     prjfile = arcpy.SpatialReference(self.spatial_ref)
-    #     arcpy.DefineProjection_management(self.outfile_shp, prjfile)
-    #     sr = arcpy.Describe(self.outfile_shp).spatialReference
-    #     print "Your ouput feature calss is : {0}".format(self.output_feature_class)
-    #     print "Your spatial reference code corresponds to: {0}".format(sr.name)
-    #
-    #     # Create projected outfile
-    #     arcpy.Project_management(self.outfile_shp, self.output_feature_class, self.spatial_ref)
+    def define_and_project(self, ofc):
+        self.output_feature_class = ofc
+        """Define spatial projection and create projected outfile"""
+        prjfile = arcpy.SpatialReference(self.spatial_ref)
+        arcpy.DefineProjection_management(self.outfile_shp, prjfile)
+        sr = arcpy.Describe(self.outfile_shp).spatialReference
+        print "Your ouput feature calss is : {0}".format(self.output_feature_class)
+        print "Your spatial reference code corresponds to: {0}".format(sr.name)
+
+        # Create projected outfile
+        arcpy.Project_management(self.outfile_shp, self.output_feature_class, self.spatial_ref)
 
 
 def find_all_csv(workspace):
@@ -189,7 +189,8 @@ outputfolder = arcpy.GetParameterAsText(8)
 denali = arcpy.GetParameterAsText(9)
 projcode = int(arcpy.GetParameterAsText(10))
 projected_covariates = arcpy.GetParameterAsText(11)
-map_document = arcpy.GetParameterAsText(12)
+join_file = arcpy.GetParameterAsText(12)
+map_document = arcpy.GetParameterAsText(13)
 denali_cov = os.path.basename(denali)[:-4] + "_covariates.shp"
 
 # Environmental Variables
@@ -248,9 +249,9 @@ try:
     proj4string(cov.b) <- proj4string(ak_proj)
     cov.ab <- spTransform(cov.b,CRS(proj4string(ak_proj)))
     ''')
-
+    arcpy.Delete_management("covariates.shp")
+    arcpy.Delete_management("covariates.lyr")
     if arcpy.Exists(projected_covariates):
-            arcpy.Delete_management("covariates.shp")
             arcpy.Delete_management(projected_covariates)
     ro.r('writeOGR(cov.ab, dsn, layer="covariates_proj", driver="ESRI Shapefile")')
 
@@ -263,11 +264,11 @@ except arcpy.AddMessage("An error occurred during processing:\n"):
 
 # Change working directory
 arcpy.env.workspace = shp_output_dir
+arcpy.gp.overwriteOutput = True
 
 # Hardcode generic spatial join file names
 denali_base = os.path.basename(denali)
-join_file = "spatial_join.shp"
-join_proj_file = "spatial_join_proj.shp"
+#join_proj_file = "spatial_join_proj.shp"
 #
 # # Perform Clip
 clip_features(projected_covariates, denali, denali_cov)
@@ -277,21 +278,34 @@ clip_features(projected_covariates, denali, denali_cov)
 # Process: Spatial Join
 myJoin = SpatialJoin(denali, denali_cov, join_file, projcode)
 myJoin.spatial_join()
-# myJoin.define_and_project(join_proj_file)
+#myJoin.define_and_project(join_proj_file)
 
 try:
+    arcpy.AddMessage("\tGeographically Weighted Regression...")
+    # Replace a layer/table view name with a path to a dataset (which can be a layer file) or create the layer/table view within the script
+# The following inputs are layers or table views: "spatial_join"
+    gwr = arcpy.GeographicallyWeightedRegression_stats(in_features=join_file,dependent_field="permafrost",
+                                                       explanatory_field="heatload;temp;slope;cti",
+                                                       out_featureclass="GeographicallyWeightedRegression",
+                                                       kernel_type="FIXED",bandwidth_method="AICc",
+                                                       distance="#",number_of_neighbors="30",
+                                                       weight_field="#",coefficient_raster_workspace="#",
+                                                       cell_size="0.306677063650721",in_prediction_locations="#",
+                                                       prediction_explanatory_field="#",out_prediction_featureclass="#")
+
+
     # Create Spatial Weights Matrix for Calculations
     # Process: Generate Spatial Weights Matrix
-    arcpy.AddMessage("\tBuilding Spatial Weights Matrix...")
-    # swm = arcpy.GenerateSpatialWeightsMatrix_stats(Input_Feature_Class="spatial_join_proj.shp",
+    # arcpy.AddMessage("\tBuilding Spatial Weights Matrix...")
+    # swm = arcpy.GenerateSpatialWeightsMatrix_stats(Input_Feature_Class="spatial_join.shp",
     #                                                Unique_ID_Field="JOIN_FID",
     #                                                Output_Spatial_Weights_Matrix_File="spatial_weights.swm",
     #                                                Conceptualization_of_Spatial_Relationships="K_NEAREST_NEIGHBORS")
-    arcpy.AddMessage("\tSpatial Weights Matrix generated...")
+    # arcpy.AddMessage("\tSpatial Weights Matrix generated...")
 
     # Exploratory Regression Analysis for permafrost
-    arcpy.AddMessage("\tPerforming Exploratory Spatial Regression...")
-    # er = arcpy.ExploratoryRegression_stats(Input_Features="spatial_join_proj.shp",
+    # arcpy.AddMessage("\tPerforming Exploratory Spatial Regression...")
+    # er = arcpy.ExploratoryRegression_stats(Input_Features="spatial_join.shp",
     #                                        Dependent_Variable="permafrost",
     #                                        Candidate_Explanatory_Variables=\
     #                                        "heatload;temp;slope;cti;texture",
@@ -303,15 +317,18 @@ try:
     #                                        Maximum_Coefficient_p_value_Cutoff="0.10",
     #                                        Maximum_VIF_Value_Cutoff="7.5")
     # Move results.txt to data folder
-    if arcpy.Exists( os.path.join(shp_output_dir, "results.txt")):
-        shutil.move( os.path.join(shp_output_dir, "results.txt"),
-                     os.path.join(dir_string, "regression_results\\results.txt"))
+    # if arcpy.Exists( os.path.join(shp_output_dir, "results.txt")):
+    #     shutil.move( os.path.join(shp_output_dir, "results.txt"),
+    #                  os.path.join(dir_string, "regression_results\\results.txt"))
+    #
+    # if arcpy.Exists( os.path.join(shp_output_dir, "results.txt.xml")):
+    #     shutil.move( os.path.join(shp_output_dir, "results.txt.xml"),
+    #                  os.path.join(dir_string, "regression_results\\results.txt.xml"))
 
-    if arcpy.Exists( os.path.join(shp_output_dir, "results.txt.xml")):
-        shutil.move( os.path.join(shp_output_dir, "results.txt.xml"),
-                     os.path.join(dir_string, "regression_results\\results.txt.xml"))
+    if arcpy.Exists(join_file):
+        arcpy.Delete_management(join_file)
 
-    arcpy.AddMessage("Regression Analysis Complete.")
+    arcpy.AddMessage("Geographically Weighted Regression Analysis Complete.")
 
 except arcpy.ExecuteError("An error occurred during processing:\n"):
     msgs = arcpy.GetMessages(2)
